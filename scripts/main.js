ee869d1d08213dc6b2109a063e6957ea675a08d0
Lynn Samuelson <lynn.samuelson@gmail.com>
2021-11-22 18:45:03 -0600

    set-up basic html and js

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const maincontainer = document.getElementById("app");
+
+const html = `<h1>E17's Quote Book</h1>`
+maincontainer.innerHTML = html
+
+// To start lines 3&4 could be just 1 line of code
+//maincontainer.innerHTML = `<h1>E17's Quote Book</h1>`
